d1d1618cf9062e37f544b5a3749fedc749e4242d
alexchung <yonganzhong@outlook.com>
2020-09-29 20:24:07 +0800

    update model

--- libs/nets/model.py
+++ libs/nets/model.py
@@ -17,10 +17,14 @@ class Generator(tf.keras.Model):
     def __init__(self):
         super(Generator, self).__init__()
 
-        # (-1, 100) => (-1, 7*7*256)
-        self.fc1 = tf.keras.layers.Dense(units=7 * 7 * 256, use_bias=False, input_dim=(100,))
+        self.fc1 = tf.keras.layers.Dense(units=512, use_bias=False, input_dim=(100,))
         self.bn1 = tf.keras.layers.BatchNormalization()
         self.leak_relu1 = tf.keras.layers.LeakyReLU()
+
+        # (-1, 100) => (-1, 7*7*256)
+        self.fc2 = tf.keras.layers.Dense(units=7 * 7 * 256, use_bias=False, input_dim=(512,))
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.leak_relu2 = tf.keras.layers.LeakyReLU()
 
         # (-1, 7*7*256) => (-1, 7, 7, 256)
         self.reshape = tf.keras.layers.Reshape((7, 7, 256))
@@ -28,14 +32,14 @@ class Generator(tf.keras.Model):
         # transpose convd (-1, 7, 7, 256) => (-1, 7, 7, 128)
         self.conv_trans1 = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=(5, 5), strides=(1, 1),
                                                            padding='same', use_bias=False)
-        self.bn2 = tf.keras.layers.BatchNormalization()
-        self.leak_relu2 = tf.keras.layers.LeakyReLU()
+        self.bn3 = tf.keras.layers.BatchNormalization()
+        self.leak_relu3 = tf.keras.layers.LeakyReLU()
 
         # transpose convd (-1, 7, 7, 128) => (-1, 14, 14, 64)
         self.conv_trans2 = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=(5, 5), strides=(2, 2),
                                                            padding='same', use_bias=False)
-        self.bn3 = tf.keras.layers.BatchNormalization()
-        self.leak_relu3 = tf.keras.layers.LeakyReLU()
+        self.bn4 = tf.keras.layers.BatchNormalization()
+        self.leak_relu4 = tf.keras.layers.LeakyReLU()
 
         # transpose convd (-1, 14, 14, 64) => (-1, 28, 28, 1)
         self.conv_trans3 = tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=(5, 5), strides=(2, 2),
@@ -48,10 +52,14 @@ class Generator(tf.keras.Model):
         """
 
         # assert noise.shape == (cfgs.BATCH_SIZE, cfgs.Z_DIM)
-        # full connect block
+
         model = self.fc1(noise)
         model = self.bn1(model)
         model = self.leak_relu1(model)
+        # full connect block
+        model = self.fc2(model)
+        model = self.bn2(model)
+        model = self.leak_relu2(model)
         # assert model.shape == (None, 7*7*256)
 
         # reshape
@@ -60,14 +68,14 @@ class Generator(tf.keras.Model):
 
         # transpose convolution block
         model = self.conv_trans1(model)
-        model = self.bn2(model)
-        model = self.leak_relu2(model)
+        model = self.bn3(model)
+        model = self.leak_relu3(model)
         # assert model.shape == (None, 7, 7, 128)
 
         # transpose convolution block
         model = self.conv_trans2(model)
-        model = self.bn3(model)
-        model = self.leak_relu3(model)
+        model = self.bn4(model)
+        model = self.leak_relu4(model)
         # assert model.shape == (None, 14, 14, 64)
 
         # transpose convolution block
